704149489ebd767448725fea6cd95b848ce78eb8
apsar <apsarpasha049@gmail.com>
2022-09-29 09:59:27 +0530

    pushing footer

---
+++ node.js
@@ -0,0 +1,17 @@
+function getElementAt(qIndex, listHead) {
+    //   Node.data = qIndex;
+    let temp = listHead;
+    let val = [];
+    while (temp.next != null) {
+      val.push(temp.data);
+      temp = temp.next;
+    }
+    if (temp.next === null) {
+      val.push(temp.data);
+    }
+    if (val[qIndex - 1] === undefined) {
+      return -1;
+    } else {
+      return val[qIndex - 1];
+    }
+  }
